d2f87fecd4fdfb8f4a0c4a335dc80ca69eeb18df
Aulia Ramadhan <auliarama965@gmail.com>
2023-12-17 13:55:18 +0700

    Initial Commit

--- src/app/page.js
+++ src/app/page.js
@@ -1,113 +1,123 @@
-import Image from 'next/image'
+"use client";
+import Image from "next/image";
+import Atropos from "atropos/react";
+import "atropos/css";
+import AudioPlayer from "react-h5-audio-player";
+import "react-h5-audio-player/lib/styles.css";
+import Marquee from "react-fast-marquee";
+import { useEffect, useState } from "react";
+import { FaGithub, FaInstagramSquare } from "react-icons/fa";
+import { Poppins } from "next/font/google";
+import Link from "next/link";
+
+const poppins = Poppins(
+  {
+    subsets: ['devanagari'],
+    weight: ["400", "600", "700", "800"],
+    style: "normal"
+  }
+)
+
+// shadow-[5px_5px_0px_0px_rgba(109,40,217)]
 
 export default function Home() {
+  const [time, setTime] = useState(new Date());
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setTime(new Date());
+    }, 1000);
+
+    return () => clearInterval(interval);
+  }, []);
+
+  const hours = time.getHours();
+  const minutes = time.getMinutes();
+  const seconds = time.getSeconds();
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/app/page.js</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+    <main class={poppins.className} className="w-full h-screen flex justify-center align-middle items-center bg-[#FEF6DA]">
+      <Atropos>
+        <div class="w-[350px] h-auto md:w-[600px] md:h-[650px] border-black border-4 rounded-md p-8 bg-white ">
+          {/* section container top */}
+          <section className="flex flex-col md:flex-row h-auto gap-6">
+            {/* left section */}
+            <div className="w-full flex flex-col gap-7 md:flex rounded-lg">
+              <div
+                data-atropos-offset="5"
+                className="h-full rounded-lg flex items-center justify-center shadow-[5px_5px_0px_0px_rgba(000,00,000)] bg-[#A0CCD3]"
+              >
+                <p
+                  data-atropos-offset="10"
+                  className="text-[#ffffff] font-bold text-4xl"
+                >{`${hours}:${minutes}:${seconds}`}</p>
+              </div>
+
+              <div
+                data-atropos-offset="5"
+                className="hidden md:flex h-full rounded-lg  shadow-[5px_5px_0px_0px_rgba(000,00,000)] bg-[#A0CCD3]"
+              >
+                tes
+              </div>
+
+              <div className="flex h-full gap-5 ">
+               <Link data-atropos-offset="10" className="w-full p-3 flex items-center justify-center shadow-[5px_5px_0px_0px_rgba(000,00,000)] bg-[#A0CCD3] rounded-lg" href="https://www.instagram.com/ul.ramadhan" target="_blank">
+               <div
+                >
+                  <FaInstagramSquare
+                    data-atropos-offset="10"
+                    className="text-5xl"
+                  />
+                </div>
+               </Link>
+               <Link data-atropos-offset="10"
+                  className="w-full p-3 flex items-center justify-center shadow-[5px_5px_0px_0px_rgba(000,00,000)] bg-[#A0CCD3] rounded-lg" href="https://github.com/sayarama" target="_blank">
+               <div
+                >
+                  <FaGithub data-atropos-offset="10" className="text-5xl" />
+                </div>
+               </Link>
+              </div>
+            </div>
+            {/* right section */}
+            <div
+              data-atropos-offset="5"
+              className="w-full p-5 text-center gap-3 items-center flex flex-col shadow-[5px_5px_0px_0px_rgba(000,00,000)] bg-[#A0CCD3] rounded-lg"
+            >
+              <div
+                data-atropos-offset="5"
+                className="bg-[url('/images/cover.jpg')] bg-cover bg-no-repeat bg-center w-full rounded-tl-lg rounded-tr-lg h-32"
+              ></div>
+              <p data-atropos-offset="5" className="font-medium">Gyakutengeki</p>
+              <div data-atropos-offset="5">
+                <AudioPlayer className="rounded-md " src="/assets/music.ogg" />
+              </div>
+            </div>
+          </section>
+
+          {/* Section container bottom */}
+          <section className="mt-8">
+            <div
+              data-atropos-offset="5"
+              className="w-full h-48 p-8 shadow-[5px_5px_0px_0px_rgba(000,00,000)] bg-[#A0CCD3] rounded-lg"
+            >
+              Hello, I'm Enka, also known as Rama or Aul or something else. So,
+              what's the purpose of this website? No one knows, I don't even know,
+              and no one will ever know ...
+            </div>
+            <div
+              data-atropos-offset="5"
+              className="w-full mt-5 text-center shadow-[5px_5px_0px_0px_rgba(000,00,000)] bg-[#A0CCD3] rounded-md"
+            >
+              <Marquee speed={100}>
+                <p className="text-xl font-bold ">
+                  © Enka | No animals were harmed in the making of this website,
+                  but a few pixels may have gotten slightly bruised.
+                </p>
+              </Marquee>
+            </div>
+          </section>
         </div>
-      </div>
-
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800 hover:dark:bg-opacity-30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore starter templates for Next.js.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
+      </Atropos>
     </main>
-  )
+  );
 }
